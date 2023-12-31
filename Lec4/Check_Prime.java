2d3d1686be33fcf09ca4a52b7f6b335051d29807
monukumar98 <monu012as@gmail.com>
2023-12-31 12:20:20 +0530

    Lops Qps

---
+++ Lec4/Check_Prime.java
@@ -0,0 +1,25 @@
+package Lec4;
+
+import java.util.Scanner;
+
+public class Check_Prime {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int fact = 0;
+		for (int i = 2; i < n; i++) {
+			if (n % i == 0) {
+				fact++;
+			}
+		}
+		if (fact >= 1) {
+			System.out.println("Not Prime");
+		} else {
+			System.out.println("Prime");
+		}
+
+	}
+
+}
---
+++ Lec4/For_Demo_Loop.java
@@ -0,0 +1,33 @@
+package Lec4;
+
+public class For_Demo_Loop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+//		int i = 1;
+//		while (i <= 5) {
+//			// self work
+//
+//			i++;
+//		}
+		for (int i = 1; i <= 5; i++) {
+			if(i==2) {
+				continue;
+			}
+			System.out.println(i);
+
+		}
+//		int j = 1;
+//		for (; j <= 5; j++) {
+//			System.out.println(j);
+//
+//		}
+//		
+//		for (int k = 1; k <= 5; ) {
+//			System.out.println(j);
+//			k++;
+//		}
+	}
+
+}
